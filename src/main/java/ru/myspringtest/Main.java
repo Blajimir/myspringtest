5b14594946a69c5e472721204972b0e2ce41a206
Blajimir <blajimir@gmail.com>
2017-02-10 09:52:29 +0300

    new project for experiments

---
+++ src/main/java/ru/myspringtest/Main.java
@@ -0,0 +1,10 @@
+package ru.myspringtest;
+
+/**
+ * Created by blajimir on 09.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/myspringtest/config/AppConfig.java
@@ -0,0 +1,12 @@
+package ru.myspringtest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by blajimir on 09.02.2017.
+ */
+@Configuration
+//@ComponentScan
+public class AppConfig {
+}
